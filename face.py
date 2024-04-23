070f1ee5614ecd3e9de626e549287a2c2fabfc21
markb2575 <markbassily2575@gmail.com>
2024-04-23 19:00:48 -0400

    added neural network

--- face.py
+++ face.py
@@ -39,7 +39,7 @@ def getFeatures(x):
             features.append(np.count_nonzero(range_data == 1))
     return features
 
-def train(data, labels):
+def trainPerceptron(data, labels):
     """
     This method computes trains for 10 minutes then returns the weights
 
@@ -70,7 +70,7 @@ def train(data, labels):
         #     break
     return weights
 
-def test(data, labels, weights):
+def testPerceptron(data, labels, weights):
     totalCorrect = 0
     for face_num in range(len(data)):
         features = getFeatures(data[face_num])
@@ -78,18 +78,81 @@ def test(data, labels, weights):
         for i in range(42):
             output += weights[i] * features[i]
         output += weights[42]
-        if (output >= 0 and labels[face_num] == 1):
-            totalCorrect +=1
-        elif (output < 0 and labels[face_num] == 0):
+        if ((output >= 0 and labels[face_num] == 1) or (output < 0 and labels[face_num] == 0)):
             totalCorrect +=1
     return totalCorrect/len(data)
 
 def perceptron():
     faceDataTrain, faceDataTrainLabels = getFaceData(1)
-    weights = train(faceDataTrain, faceDataTrainLabels)
+    weights = trainPerceptron(faceDataTrain, faceDataTrainLabels)
     faceDataTest, faceDataTestLabels = getFaceData(0)
-    print(weights)
-    accuracy = test(faceDataTest, faceDataTestLabels, weights)
-    print(accuracy)
+    # print(weights)
+    accuracy = testPerceptron(faceDataTest, faceDataTestLabels, weights)
+    print(f"Accuracy for Perceptron: {round((accuracy) * 100, 2)}%")
+
+def testNeural(data, labels, weights):
+    w_i_h, w_h_o, b_i_h, b_h_o = weights
+    totalCorrect = 0
+    for face_num in range(len(data)):
+        features = getFeatures(data[face_num])
+        features = np.array(features)
+        label = np.array(labels[face_num])
+        features.shape += (1,)
+        h_pre = b_i_h + w_i_h @ features
+        h = 1 / (1 + np.exp(-h_pre))
+        o_pre = b_h_o + w_h_o @ h
+        o = 1 / (1 + np.exp(-o_pre))
+        if ((label == 1 and o >= 0.5) or (label == 0 and o < 0.5)):
+            totalCorrect += 1
+    return totalCorrect/len(data)
+
+def trainNeural(data, labels):
+    w_i_h = np.random.uniform(-1,1, (20,42))
+    w_h_o = np.random.uniform(-1,1, (1,20))
+    b_i_h = np.zeros((20, 1))
+    b_h_o = np.zeros((1, 1))
+    learn_rate = 0.01
+    nr_correct = 0
+    start_time = time.time()
+    while True:
+        for face_num in range(len(data)):
+            features = getFeatures(data[face_num])
+            features = np.array(features)
+            label = np.array(labels[face_num])
+            features.shape += (1,)
+
+            h_pre = b_i_h + w_i_h @ features
+            h = 1 / (1 + np.exp(-h_pre))
+
+            o_pre = b_h_o + w_h_o @ h
+            o = 1 / (1 + np.exp(-o_pre))
+
+            e = 1 / len(o) * np.sum((o - label) ** 2, axis=0)
+            if ((label == 1 and o >= 0.5) or (label == 0 and o < 0.5)):
+                nr_correct += 1
+
+            delta_o = o - label
+
+            w_h_o += -learn_rate * delta_o @ np.transpose(h)
+            b_h_o += -learn_rate * delta_o
+
+            delta_h = np.transpose(w_h_o) @ delta_o * (h * (1 - h))
+            w_i_h += -learn_rate * delta_h @ np.transpose(features)
+            b_i_h += -learn_rate * delta_h
+        # print(f"{nr_correct / len(data)} {nr_correct} {len(data)}")
+        nr_correct = 0
+        if time.time() - start_time >= 600: # if 10 minutes has passed break out of loop
+            break
+    return w_i_h, w_h_o, b_i_h, b_h_o
+
+
+def neural():
+    faceDataTrain, faceDataTrainLabels = getFaceData(1)
+    weights = trainNeural(faceDataTrain, faceDataTrainLabels)
+    faceDataTrain, faceDataTrainLabels = getFaceData(0)
+    # print(weights)
+    accuracy = testNeural(faceDataTrain, faceDataTrainLabels, weights)
+    print(f"Accuracy for Neural Network: {round((accuracy) * 100, 2)}%")
 
 perceptron()
+neural()
