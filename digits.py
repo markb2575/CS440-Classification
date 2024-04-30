4384db8336983c844c71d1b41597f53c43ee0d68
markb2575 <markbassily2575@gmail.com>
2024-04-30 03:16:44 -0400

    added neural network for digits

--- digits.py
+++ digits.py
@@ -110,68 +110,71 @@ def testPerceptron(data, labels, weights):
             totalCorrect +=1
     return totalCorrect/len(data)
 
-# def testNeural(data, labels, weights):
-#     w_i_h, w_h_o, b_i_h, b_h_o = weights
-#     totalCorrect = 0
-#     for digits_num in range(len(data)):
-#         features = getFeatures(data[digits_num])
-#         features = np.array(features)
-#         label = np.array(labels[digits_num])
-#         features.shape += (1,)
-#         h_pre = b_i_h + w_i_h @ features
-#         h = 1 / (1 + np.exp(-h_pre))
-#         o_pre = b_h_o + w_h_o @ h
-#         o = 1 / (1 + np.exp(-o_pre))
-#         if ((label == 1 and o >= 0.5) or (label == 0 and o < 0.5)):
-#             totalCorrect += 1
-#     return totalCorrect/len(data)
+def testNeural(data, labels, weights):
+    w_i_h, w_h_o, b_i_h, b_h_o = weights
+    totalCorrect = 0
+    for digits_num in range(len(data)):
+        features = getFeatures(data[digits_num])
+        features = np.array(features)
+        label = np.array(labels[digits_num])
+        features.shape += (1,)
+        h_pre = b_i_h + w_i_h @ features
+        
+        h = 1 / (1 + np.exp(-h_pre))
+        
+        o_pre = b_h_o + w_h_o @ h
+        
+        o = 1 / (1 + np.exp(-o_pre))
+        # print(o)
+        # print(np.argmax(o))
+        # print(label)
+        # print()
+        if (np.argmax(o) == label):
+            totalCorrect += 1
+    return totalCorrect/len(data)
 
-# def trainNeural(data, labels):
-#     w_i_h = np.random.uniform(-1,1, (20,49))
-#     w_h_o = np.random.uniform(-1,1, (10,20))
-#     b_i_h = np.zeros((20, 1))
-#     b_h_o = np.zeros((10, 1))
-#     learn_rate = 0.01
-#     highestAccuracy = 0
-#     consecutiveBelow = 0
-#     start_time = time.time()
-#     digitsDataTest, digitsDataTestLabels = getDigitsData(0)
-#     while True:
-#         for digits_num in range(len(data)):
-#             features = getFeatures(data[digits_num])
-#             features = np.array(features)
-#             label = np.array(labels[digits_num])
-#             features.shape += (1,)
+def trainNeural(data, labels):
+    w_i_h = np.random.uniform(-1,1, (30,49))
+    w_h_o = np.random.uniform(-1,1, (10,30))
+    b_i_h = np.zeros((30, 1))
+    b_h_o = np.zeros((10, 1))
+    learn_rate = 0.01
+    highestAccuracy = 0
+    consecutiveBelow = 0
+    digitsDataTest, digitsDataTestLabels = getDigitsData(0)
+    while True:
+        for digits_num in range(len(data)):
+            features = getFeatures(data[digits_num])
+            features = np.array(features)
+            label = np.array(labels[digits_num])
+            label = np.eye(10)[label]
+            label = np.array(label)
+            label.shape += (1,)
+            features.shape += (1,)
 
-#             h_pre = b_i_h + w_i_h @ features
-#             h = 1 / (1 + np.exp(-h_pre))
+            h_pre = b_i_h + w_i_h @ features
+            h = 1 / (1 + np.exp(-h_pre))
 
-#             o_pre = b_h_o + w_h_o @ h
-#             o = 1 / (1 + np.exp(-o_pre))
+            o_pre = b_h_o + w_h_o @ h
+            o = 1 / (1 + np.exp(-o_pre))
+            delta_o = o - label
+            w_h_o += -learn_rate * delta_o @ np.transpose(h)
+            b_h_o += -learn_rate * delta_o
 
-
-#             # if ((label == 1 and o >= 0.5) or (label == 0 and o < 0.5)):
-#             #     nr_correct += 1
-
-#             delta_o = o - label
-
-#             w_h_o += -learn_rate * delta_o @ np.transpose(h)
-#             b_h_o += -learn_rate * delta_o
-
-#             delta_h = np.transpose(w_h_o) @ delta_o * (h * (1 - h))
-#             w_i_h += -learn_rate * delta_h @ np.transpose(features)
-#             b_i_h += -learn_rate * delta_h
-#         weights = w_i_h, w_h_o, b_i_h, b_h_o
-#         testResult = testNeural(digitsDataTest,digitsDataTestLabels,weights)
-#         # print(testResult)
-#         if testResult >= highestAccuracy:
-#             highestAccuracy = testResult
-#             consecutiveBelow = 0
-#         else:
-#             consecutiveBelow+=1
-#         if consecutiveBelow >= 20:
-#             break
-#     return w_i_h, w_h_o, b_i_h, b_h_o
+            delta_h = np.transpose(w_h_o) @ delta_o * (h * (1 - h))
+            w_i_h += -learn_rate * delta_h @ np.transpose(features)
+            b_i_h += -learn_rate * delta_h
+        weights = w_i_h, w_h_o, b_i_h, b_h_o
+        testResult = testNeural(digitsDataTest,digitsDataTestLabels,weights)
+        # print(testResult)
+        if testResult >= highestAccuracy:
+            highestAccuracy = testResult
+            consecutiveBelow = 0
+        else:
+            consecutiveBelow+=1
+        if consecutiveBelow >= 15:
+            break
+    return w_i_h, w_h_o, b_i_h, b_h_o
 
 def splitDataPoints(data, labels):
     dataWithLabels = list(zip(data, labels))
@@ -180,30 +183,30 @@ def splitDataPoints(data, labels):
             random.sample(dataWithLabels, round(size * .5)), random.sample(dataWithLabels, round(size * .6)), random.sample(dataWithLabels, round(size * .7)), random.sample(dataWithLabels, round(size * .8)), 
             random.sample(dataWithLabels, round(size * .9)), dataWithLabels)
 
-# def neural(training):
-#     if training:
-#         digitsDataTrain, digitsDataTrainLabels = getDigitsData(1)
-#         splits = splitDataPoints(digitsDataTrain, digitsDataTrainLabels)
-#         weights = []
-#         for split in splits:
-#             data, label = zip(*split)
-#             weight = trainNeural(data,label)
-#             weights.append(weight)
-#             print("done")
-#         return weights
-#     else:
-#         digitsDataTest, digitsDataTestLabels = getDigitsData(0)
-#         percent = 10
-#         while percent <= 100:
-#             w_i_h = np.load("weights/neural_digits/" + str(percent) + "%/w_i_h.npy")
-#             w_h_o = np.load("weights/neural_digits/" + str(percent) + "%/w_h_o.npy")
-#             b_i_h = np.load("weights/neural_digits/" + str(percent) + "%/b_i_h.npy")
-#             b_h_o = np.load("weights/neural_digits/" + str(percent) + "%/b_h_o.npy")
-#             weights = w_i_h, w_h_o, b_i_h, b_h_o
-#             accuracy = testNeural(digitsDataTest, digitsDataTestLabels, weights)
-#             print(f"Accuracy for Neural Network with {percent}% Training Data: {round((accuracy) * 100, 2)}%")
-#             percent += 10
-#         print()
+def neural(training):
+    if training:
+        digitsDataTrain, digitsDataTrainLabels = getDigitsData(1)
+        splits = splitDataPoints(digitsDataTrain, digitsDataTrainLabels)
+        weights = []
+        for split in splits:
+            data, label = zip(*split)
+            weight = trainNeural(data,label)
+            weights.append(weight)
+            print("done")
+        return weights
+    else:
+        digitsDataTest, digitsDataTestLabels = getDigitsData(0)
+        percent = 10
+        while percent <= 100:
+            w_i_h = np.load("weights/neural_digits/" + str(percent) + "%/w_i_h.npy")
+            w_h_o = np.load("weights/neural_digits/" + str(percent) + "%/w_h_o.npy")
+            b_i_h = np.load("weights/neural_digits/" + str(percent) + "%/b_i_h.npy")
+            b_h_o = np.load("weights/neural_digits/" + str(percent) + "%/b_h_o.npy")
+            weights = w_i_h, w_h_o, b_i_h, b_h_o
+            accuracy = testNeural(digitsDataTest, digitsDataTestLabels, weights)
+            print(f"Accuracy for Neural Network with {percent}% Training Data: {round((accuracy) * 100, 2)}%")
+            percent += 10
+        print()
 
 
 def perceptron(training):
@@ -230,24 +233,24 @@ def perceptron(training):
         print()
 
 training = False
-perceptronWeights = perceptron(training=training)
-# neuralWeights = neural(training=training)
+# perceptronWeights = perceptron(training=training)
+neuralWeights = neural(training=training)
 
 if training:
     percent = 10
-    # for weight in neuralWeights:
-    #     w_i_h, w_h_o, b_i_h, b_h_o = weight
-    #     np.save("weights/neural_digits/" + str(percent) + "%/w_i_h", w_i_h)
-    #     np.save("weights/neural_digits/" + str(percent) + "%/w_h_o", w_h_o)
-    #     np.save("weights/neural_digits/" + str(percent) + "%/b_i_h", b_i_h)
-    #     np.save("weights/neural_digits/" + str(percent) + "%/b_h_o", b_h_o)
-    #     percent += 10
-
-
-    percent = 10
-    for weight in perceptronWeights:
-        for i in range(10):
-            np.save("weights/perceptron_digits/" + str(percent) + "%/" + str(i), weight[i])
+    for weight in neuralWeights:
+        w_i_h, w_h_o, b_i_h, b_h_o = weight
+        np.save("weights/neural_digits/" + str(percent) + "%/w_i_h", w_i_h)
+        np.save("weights/neural_digits/" + str(percent) + "%/w_h_o", w_h_o)
+        np.save("weights/neural_digits/" + str(percent) + "%/b_i_h", b_i_h)
+        np.save("weights/neural_digits/" + str(percent) + "%/b_h_o", b_h_o)
         percent += 10
+
+
+    # percent = 10
+    # for weight in perceptronWeights:
+    #     for i in range(10):
+    #         np.save("weights/perceptron_digits/" + str(percent) + "%/" + str(i), weight[i])
+    #     percent += 10
 
 
